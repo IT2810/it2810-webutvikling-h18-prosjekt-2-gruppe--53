e07e9ef3c8dfdce7cc6ee43345c5fb3210416507
Håvard Aasmo <havi95@gmail.com>
2018-09-18 11:51:33 +0200

    Remove default logo

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from '../../logo.svg';
 import './App.css';
 import CatSelect from '../CatSelect/CatSelect';
 import TabSelect from '../TabSelect/TabSelect';
@@ -10,7 +9,6 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Prosjekt 2</h1>
         </header>
         <CatSelect />
