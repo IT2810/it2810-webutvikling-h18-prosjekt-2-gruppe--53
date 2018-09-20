7e7d8983732cc244ad43ee789949494b5590c304
Håvard Aasmo <havi95@gmail.com>
2018-09-21 01:13:25 +0200

    Add stateless media components, resolve #17

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import './Content.css';
 
+const Image = ({image}) => (    
+    <div dangerouslySetInnerHTML={{__html: image}} />
+);
+
+const Text = ({text}) => (
+    <p>{text}</p>
+);
+
+const Audio = ({audio}) => (
+    <audio src={audio} controls />
+);
+
 class Content extends Component {
     render() {
         return (
-            <div>Content</div>
+            <div>
+                <Image image={this.props.image} />
+                <Text text={this.props.text} />
+                <Audio audio={this.props.audio} />
+            </div>
         );
     }
 }
