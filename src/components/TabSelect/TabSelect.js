45b420367d811af2490c9e4bb747a33154644a6a
Håvard Aasmo <havi95@gmail.com>
2018-09-21 03:02:35 +0200

    Implement tab selection, resolve #16

--- src/components/TabSelect/TabSelect.js
+++ src/components/TabSelect/TabSelect.js
@@ -2,9 +2,30 @@ import React, { Component } from 'react';
 import './TabSelect.css';
 
 class TabSelect extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(index) {
+        this.props.callback(index);
+    }
+
     render() {
+
+        let tabs = [];
+        for (let i = 0; i < 4; i++) {
+            const isActive = this.props.selected === i;
+            tabs.push(
+                <div onClick={() => this.handleClick(i)} className={['tab', isActive ? 'tab-active' : ''].join(' ')} key={i}>{'Tab '+(i+1)}</div>
+            );
+        }
+
         return (
-            <div>TabSelect</div>
+            <div className='tabs'>
+                {tabs}
+            </div>
         );
     }
 }
