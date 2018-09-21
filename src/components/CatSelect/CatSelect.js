548eb125075297d79ae7085f35a0e4193fa31e96
Håvard Aasmo <havi95@gmail.com>
2018-09-21 03:01:40 +0200

    Implement category selection, resolve #15

--- src/components/CatSelect/CatSelect.js
+++ src/components/CatSelect/CatSelect.js
@@ -2,9 +2,40 @@ import React, { Component } from 'react';
 import './CatSelect.css';
 
 class CatSelect extends Component {
+
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.props.callback(event.target.name, event.target.value);
+    }
+
     render() {
         return (
-            <div>CatSelect</div>
+            <div>
+                <form onChange={this.handleChange}>
+                    <fieldset>
+                        <label>Image</label>
+                        <input type='radio' value='cat0' name='image'></input>
+                        <input type='radio' value='cat1' name='image'></input>
+                        <input type='radio' value='cat2' name='image'></input>
+                    </fieldset>
+                    <fieldset>
+                        <label>Text</label>
+                        <input type='radio' value='cat0' name='text'></input>
+                        <input type='radio' value='cat1' name='text'></input>
+                        <input type='radio' value='cat2' name='text'></input>
+                    </fieldset>
+                    <fieldset>
+                        <label>Audio</label>
+                        <input type='radio' value='animals' name='audio'></input>
+                        <input type='radio' value='tools' name='audio'></input>
+                        <input type='radio' value='vehicles' name='audio'></input>
+                    </fieldset>
+                </form>
+            </div>
         );
     }
 }
